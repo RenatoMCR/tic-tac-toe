7aa922a6e799ce8b668396346c00130d0792817c
Renato <nadevvedan@gmail.com>
2022-01-01 18:11:33 -0300

    oh wow a update

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 
 #define P1 'X'
 #define P2 'O'
-#define grid_size 3
+#define grid_size 5
 
 void DrawBoard();
 int CheckWin();
@@ -31,7 +31,7 @@ int main(){
 		DrawBoard();
 
 		int column, line;
-		
+
 		printf("\nPlayer %d turn! (%c)\nPosition: ", turn, symbols[turn]);
 		scanf("%d %d", &line, &column);
 
@@ -39,9 +39,19 @@ int main(){
 			continue;
 
 		grid[line - 1][column - 1] = symbols[turn];
-		
-		if (CheckWin() == 1) {
-			printf("%sPlayer %d(%c) is the winner!%s", ANSI_COLOR_GREEN, turn, symbols[turn], ANSI_COLOR_RESET);
+
+		if (CheckWin() == 1){
+			printf("%sPlayer %d(%c) is the winner!%s\nPlay again? (y/...)\n", ANSI_COLOR_GREEN, turn, symbols[turn], ANSI_COLOR_RESET);
+
+			char awnser;
+			scanf(" %c", &awnser);
+
+			if (awnser == 'y'){
+				memset(grid, ' ', sizeof(grid));
+				turn = 1;
+				
+				continue;
+			}
 
 			break;
 		}
@@ -64,29 +74,28 @@ void DrawBoard(){
 			}
 
 			printf(" %c ", grid[i][j]);
-			
+
 			if (j != grid_size - 1)
 				printf("%s|", ANSI_COLOR_MAGENTA);
 		}
 
-		if (i != grid_size - 1)
-			printf("\n%s----------\n", ANSI_COLOR_RED);
-		
+		if (i != grid_size - 1) {
+			printf("\n");
+
+			for (int x = 0; x <= grid_size; x++)
+				printf("---");
+
+			printf("\n");
+		}
+
 		printf(ANSI_COLOR_RESET);
 	}
 }
 
-//  3 | 3 | 3 
-//  ----------
-//  3 | 3 | 3 
-//  ----------
-//  3 | 3 | 3 
-
-
 int CheckWin(){
 	int horizontal = 0, vertical = 0;
 	int diagonal_left = 0, diagonal_right = 0;
-	
+
 	for (int i = 0; i < grid_size; i++){
 		for (int j = 0; j < grid_size; j++){
 			if (grid[i][j] == symbols[turn])
@@ -105,7 +114,7 @@ int CheckWin(){
 		if (grid[i][i] == symbols[turn])
 			diagonal_left++;
 		if (grid[j][j] == symbols[turn])
-			diagonal_right--; 
+			diagonal_right--;
 
 		if (diagonal_left == grid_size || diagonal_right == grid_size)
 			return 1;
